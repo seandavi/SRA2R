b2860e3d5f7d3c0935e857c4d541d512926bfa62
Sean Davis <seandavi@gmail.com>
2016-06-07 10:42:59 -0400

    Working installation of NGS-SDK in container

--- inst/docker/install_ngs_sdk.sh
+++ inst/docker/install_ngs_sdk.sh
@@ -5,6 +5,20 @@ cd ncbi
 git clone https://github.com/ncbi/ngs.git
 git clone https://github.com/ncbi/ncbi-vdb.git
 cd ngs
+echo "********************************************"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*          Making NGS-SDK                  *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "********************************************"
+
 ./configure
 make -C ngs-sdk
 make -C ngs-sdk install
@@ -16,8 +30,23 @@ cd ..
 make -C ngs-bam
 make -C ngs-bam install
 
+echo "********************************************"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*          Making NCBI-VCB                 *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "*                                          *"
+echo "********************************************"
+
+
 # NCBI-VDB
-cd ncbi-vdb/
+cd ../ncbi-vdb/
 ./configure
 make
 make install
